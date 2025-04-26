a1a6156f01c6a1095a6da5ac2b2e1fa319c374fc
AjdinMehmedovic <ajdin.mehmedovic@htecgroup.com>
2025-04-26 14:08:20 +0200

    Section 4 complete, JWT, Authentication basic, IdentityServices, Extension methods, etc

--- API/Program.cs
+++ API/Program.cs
@@ -1,18 +1,11 @@
-using API.Data;
-using Microsoft.EntityFrameworkCore;
+using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
-builder.Services.AddCors();
+builder.Services.AddApplicationServices(builder.Configuration);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddIdentityServices(builder.Configuration);
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -25,10 +18,12 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
 
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200",
 "https://localhost:4200"));
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
