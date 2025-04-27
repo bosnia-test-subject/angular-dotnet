688174721ccd8693107441e10801c4fd6ffd0d5a
AjdinMehmedovic <ajdin.mehmedovic@htecgroup.com>
2025-04-27 11:32:13 +0200

    Section 6 ruuting complete, bootswatch, auth guards, routeguards, dummy routes, toastr service

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -3,31 +3,35 @@ import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule, BsDropdownModule],
+  imports: [FormsModule, BsDropdownModule, RouterLink, RouterLinkActive, TitleCasePipe],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
+  private router = inject(Router);
   accountService = inject(AccountService);
+  private toastr = inject(ToastrService);
   model: any = {}
 
   login() {
     this.accountService.login(this.model).subscribe({
-      next: response => {
-        console.log('Login successful', response);
+      next: _ => {
+        this.router.navigateByUrl('/members')
       },
-      error: error => {
-        console.log('Login error', error);
-      }
+      error: error => this.toastr.error(error.error),
     });
   }
 
   logout() 
   {
+    this.router.navigateByUrl('/');
     this.accountService.logout();
   }
   
