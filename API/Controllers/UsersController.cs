2e186773cd6d1ece604316668ecd5f942a5964ce
AjdinMehmedovic <ajdin.mehmedovic@htecgroup.com>
2025-04-28 16:39:05 +0200

    Automapper, configuring UsersController with MemberDtos, adding helpers folder

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,7 +1,9 @@
 using System;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -9,24 +11,26 @@ using Microsoft.EntityFrameworkCore;
 namespace API.Controllers;
 
 [Authorize]
-public class UsersController(IUserRepository userRepository) : BaseApiController
+public class UsersController(IUserRepository userRepository, IMapper mapper) : BaseApiController
 {
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() 
+    public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers() 
     {
         var users = await userRepository.GetUsersAsync();
 
-        return Ok(users);
+        var usersToReturn = mapper.Map<IEnumerable<MemberDto>>(users);
+
+        return Ok(usersToReturn);
     }
     // moramo staviti curly brackets 
     // unutar http metode zbog dynamic assigning.
     [HttpGet("{username}")] //api/users/3
-    public async Task<ActionResult<AppUser>> GetUser(string username) 
+    public async Task<ActionResult<MemberDto>> GetUser(string username) 
     {
         var user = await userRepository.GetUserByUsernameAsync(username);
 
         if (user == null) return NotFound();
 
-        return user;
+        return mapper.Map<MemberDto>(user);
     }
 }
