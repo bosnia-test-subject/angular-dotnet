027cd0a25bd74b11e154e3580af54f412cef4729
AjdinMehmedovic <ajdin.mehmedovic@htecgroup.com>
2025-04-29 11:28:08 +0200

    canDeactivete route guard functionality for member edit component and hostlistener for unsaved changes

--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -1,9 +1,10 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, HostListener, inject, OnInit, ViewChild } from '@angular/core';
 import { Member } from '../../_models/member';
 import { AccountService } from '../../_services/account.service';
 import { MembersService } from '../../_services/members.service';
 import { TabsModule } from 'ngx-bootstrap/tabs';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-member-edit',
@@ -13,9 +14,18 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './member-edit.component.css'
 })
 export class MemberEditComponent implements OnInit {
+  @ViewChild('editForm') editForm?: NgForm;
+  @HostListener('window:beforeunload', ['$event']) notify($event:any)
+  {
+    if(this.editForm?.dirty)
+      {
+        $event.returnValue = true;
+      }
+  };
   member?: Member;
   private accountService = inject(AccountService);
   private memberService = inject(MembersService);
+  private toastr = inject(ToastrService);
 
   ngOnInit(): void {
     this.loadMember();
@@ -29,6 +39,13 @@ export class MemberEditComponent implements OnInit {
       {
         next: member => this.member = member,
       });
+  }
+
+  updateMember() 
+  {
+    console.log(this.member);
+    this.toastr.success("Profile updated succesfully");
+    this.editForm?.reset(this.member);
   }
 
 }
