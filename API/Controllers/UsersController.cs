4806eeb13fa29d0c5556ae6fcf37bcb206a83c83
AjdinMehmedovic <ajdin.mehmedovic@htecgroup.com>
2025-04-29 11:43:24 +0200

    New update member dto and httpput method for upadting users

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Security.Claims;
 using API.Data;
 using API.DTOs;
 using API.Entities;
@@ -11,7 +12,7 @@ using Microsoft.EntityFrameworkCore;
 namespace API.Controllers;
 
 [Authorize]
-public class UsersController(IUserRepository userRepository) : BaseApiController
+public class UsersController(IUserRepository userRepository, IMapper mapper) : BaseApiController
 {
     [HttpGet]
     public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers() 
@@ -30,5 +31,25 @@ public class UsersController(IUserRepository userRepository) : BaseApiController
         if (user == null) return NotFound();
 
         return user;
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto) 
+    {
+        var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+        if(username == null) return BadRequest("No username found inside token!");
+
+        var user = await userRepository.GetUserByUsernameAsync(username);
+
+        if (user == null) return BadRequest("Could not find user!");
+
+        mapper.Map(memberUpdateDto, user);
+        // pointless line ako korisnik ne promjeni nista vrati mu se badrequest sto je i okej.
+       // userRepository.Update(user);
+
+        if(await userRepository.SaveAllAsync()) return NoContent();
+
+        return BadRequest("Failed to update the user!");
     }
 }
