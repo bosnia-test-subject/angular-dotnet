f2d0b9ba0efd58d09effd462c889c0ceac3738c2
AjdinMehmedovic <ajdin.mehmedovic@htecgroup.com>
2025-04-30 12:17:48 +0200

    form changes, form group and reactive forms setup

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,22 +1,39 @@
-import { Component, inject, input, output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject, input, OnInit, output } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
   private acountService = inject(AccountService);
   private toastr = inject(ToastrService);
   cancelRegister = output<boolean>();
   model: any = {}
 
+  registerForm: FormGroup = new FormGroup({});
+
+  initializeForm() 
+  {
+    this.registerForm = new FormGroup(
+      {
+        username: new FormControl(),
+        password: new FormControl(),
+        confirmPassword: new FormControl(),
+      })
+  }
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
   register() {
+    console.log(this.registerForm.value);
     this.acountService.register(this.model).subscribe(
       {
         next: response => {
