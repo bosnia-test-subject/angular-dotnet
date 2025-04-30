8d5e3f88cee58a8c65116b9c92d854a427dc1e2f
AjdinMehmedovic <ajdin.mehmedovic@htecgroup.com>
2025-04-30 13:52:19 +0200

    reusable text input, register form validators

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,12 +1,14 @@
-import { Component, inject, input, OnInit, output } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Component, inject, OnInit, output } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
+import { JsonPipe, NgIf } from '@angular/common';
+import { TextInputComponent } from "../_forms/text-input/text-input.component";
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, JsonPipe, NgIf, TextInputComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
@@ -22,10 +24,24 @@ export class RegisterComponent implements OnInit {
   {
     this.registerForm = new FormGroup(
       {
-        username: new FormControl(),
-        password: new FormControl(),
-        confirmPassword: new FormControl(),
-      })
+        username: new FormControl('', Validators.required),
+        password: new FormControl('', [Validators.required, Validators.minLength(4), 
+          Validators.maxLength(8)]),
+        confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')]),
+      });
+      this.registerForm.controls['password'].valueChanges.subscribe(
+        {
+          next: () => this.registerForm.controls['confirmPassword'].updateValueAndValidity()
+        })
+  }
+
+  matchValues(matchTo: string): ValidatorFn 
+  {
+    return (control: AbstractControl) => 
+      {
+        return control.value === control.parent?.get(matchTo)?.value ? null
+        : {isMatching: true};
+      };
   }
 
   ngOnInit(): void {
